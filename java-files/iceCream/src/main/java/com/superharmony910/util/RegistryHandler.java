5ebd23661610ae033b1c4d77a9cc0026b2495bc1
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-31 20:52:29 +1000

    Created 'ice_cream_block'!

--- java-files/iceCream/src/main/java/com/superharmony910/util/RegistryHandler.java
+++ java-files/iceCream/src/main/java/com/superharmony910/util/RegistryHandler.java
@@ -1,12 +1,22 @@
 package com.superharmony910.util;
 
+import com.superharmony910.icecream.Items.ItemBase;
 import com.superharmony910.icecream.icecream;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 
 public class RegistryHandler {
 
-    public static final DeferredRegister<Item> ITEMS = new DeferredRegistry<>(ForgeRegistries.ITEMS, icecream.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, icecream.MOD_ID);
 
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // Items only!
+    public static final RegistryObject<Item> ICE_CREAM_BLOCK = ITEMS.register("ice_cream_block", ItemBase::new);
 }
